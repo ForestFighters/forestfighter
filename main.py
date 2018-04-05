40eaf9a7979c5ef530fc82ef7bd1f09daba305b0
Godley <charlotte.godley@hotmail.co.uk>
2018-04-05 11:29:09 +0100

    refactor: use a class

--- main.py
+++ main.py
@@ -14,78 +14,78 @@ L1_BUTTON = 6
 L2_BUTTON = 8
 R1_BUTTON = 7
 R2_BUTTON = 9
-MODE = 7
-
 
 
 LOGGER = logging.getLogger(__name__)
 
-def rainbow():
-    pass
 
-def remote():
-    left_drive, right_drive = joystick.get_reading()
-    bot.move(left_drive, right_drive)
-
-def maze():
-    pass
-
-def follow():
-    pass
-
-MODES = {L1_BUTTON: rainbow, R1_BUTTON: remote, L2_BUTTON: maze, R2_BUTTON: follow}
-
-
-def main(amybot=True, camjambot=False):
+class Controller(object):
+    MODES = {L1_BUTTON: self.rainbow, R1_BUTTON: self.remote, L2_BUTTON: self.maze, R2_BUTTON: self.follow}
     MODE = R1_BUTTON
-    joystick = Joystick()
-    # if elsing this because the init methods of both classes do stuff with hardware, so best to only intiailise deliberately
-    if amybot:
-        bot = AmyBot()
-    else:
-        bot = CamJamBot()
-    # Re-direct our output to standard error, we need to ignore standard out to hide some nasty print statements from pygame
-    sys.stdout = sys.stderr
 
-    interval = 0.0
+    def __init__(self, cambot=False, amybot=True):
+        self.joystick = Joystick()
+        # if elsing this because the init methods of both classes do stuff with hardware, so best to only intiailise deliberately
+        if amybot:
+            self.bot = AmyBot()
+        else:
+            self.bot = CamJamBot()
 
-    # Power settings
-    voltage_in = 6.0                        # Total battery voltage to the PicoBorg Reverse
-    voltage_out = 5.0 #* 0.95                # Maximum motor voltage, we limit it to 95% to allow the RPi to get uninterrupted power
+        # Re-direct our output to standard error, we need to ignore standard out to hide some nasty print statements from pygame
+        sys.stdout = sys.stderr
 
-    # Setup the power limits
-    if voltage_out > voltage_in:
-        max_power = 1.0
-    else:
-        max_power = voltage_out / float(voltage_in)
+        interval = 0.0
 
-    # Setup pygame and wait for the joystick to become available
+        # Power settings
+        voltage_in = 6.0  # Total battery voltage to the PicoBorg Reverse
+        voltage_out = 5.0  # * 0.95                # Maximum motor voltage, we limit it to 95% to allow the RPi to get uninterrupted power
 
-    try:
-        LOGGER.info('Press CTRL+C to quit')
-        running = True
-        # Loop indefinitely
-        while running:
-            # Get the latest events from the system
-            had_event = False
-            events = pygame.event.get()
-            # Handle each event individually
-            for event in events:
-                if event.type == pygame.QUIT:
-                    # User exit
-                    running = False
-                    break
-                elif event.type == pygame.JOYBUTTONDOWN:
-                    # A button on the joystick just got pushed down
-                    MODE = event.button
-                MODES[MODE]()
-            time.sleep(interval)
+        # Setup the power limits
+        if voltage_out > voltage_in:
+            max_power = 1.0
+        else:
+            max_power = voltage_out / float(voltage_in)
 
-    except KeyboardInterrupt:
-        # CTRL+C exit, disable all drives
-        #LeftMotor.speed(0)
-        #RightMotor.speed(0)
-        LOGGER.debug('Motors off')
+    def run(self):
+        try:
+            LOGGER.info('Press CTRL+C to quit')
+            running = True
+            # Loop indefinitely
+            while running:
+                # Get the latest events from the system
+                had_event = False
+                events = pygame.event.get()
+                # Handle each event individually
+                for event in events:
+                    if event.type == pygame.QUIT:
+                        # User exit
+                        running = False
+                        break
+                    elif event.type == pygame.JOYBUTTONDOWN:
+                        # A button on the joystick just got pushed down
+                        self.mode = event.button
+                    self.modes[self.mode]()
+                time.sleep(interval)
+
+        except KeyboardInterrupt:
+            # CTRL+C exit, disable all drives
+            # LeftMotor.speed(0)
+            # RightMotor.speed(0)
+            LOGGER.debug('Motors off')
+
+    def rainbow(self):
+        pass
+
+    def remote(self):
+        left_drive, right_drive = self.joystick.get_reading()
+        self.bot.move(left_drive, right_drive)
+
+    def maze(self):
+        pass
+
+    def follow(self):
+        pass
+
 
 
 if __name__ == '__main__':
@@ -96,4 +96,4 @@ if __name__ == '__main__':
     group.add_argument("--camjambot", help="Use the camjam edubot kit", action="store_true")
     args = parser.parse_args()
 
-    main(args.amybot, args.camjambot)
+    controller = Controller(args.amybot, args.camjambot)
