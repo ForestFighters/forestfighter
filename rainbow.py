81fa638b9f4e657689226e18aa43c61993e4ddf1
Godley <charlotte.godley@hotmail.co.uk>
2018-04-05 13:48:50 +0100

    pull code into main loop

--- rainbow.py
+++ rainbow.py
@@ -5,6 +5,8 @@ import numpy as np
 import cv2
 from time import sleep
 import logging
+from imagecapture import ImageCapture
+from streamprocessor import StreamProcessor
 
 
 LOGGER = logging.getLogger(__name__)
@@ -45,18 +47,77 @@ class Rainbow(object):
         # }
 
     def rainbow(self):
+        stream = picamera.array.PiRGBArray(self.camera)
+        self.camera.capture_sequence(stream, format='bgr', use_video_port=True)
         for colour in Colours:
-            image = np.empty((640 * 480 * 3,), dtype=np.uint8)
-            self.camera.capture(image, 'bgr')
-            image = image.reshape((640, 480, 3))
-            cv2.imshow('normal', image)
-            range = cv2.inRange(image, np.array(ranges[colour]["low"]), np.array(ranges[colour]["high"]))
-            cv2.imshow('range', range)
-            cv2.waitKey(0)
+            self.process(stream.array, colour)
             # eroded = cv2.erode(range, )
             LOGGER.debug(colour)
             sleep(2)
             #self.camera.capture('foo.jpg')
             range_data = ranges[colour]
             LOGGER.debug(range_data)
+
+    def process(self, image, colour, debug=False):
+        if debug:
+            cv2.imshow('original', image)
+            cv2.waitKey(0)
+
+        # Blur the image
+        image = cv2.medianBlur(image, 5)
+        if debug:
+            cv2.imshow('blur', image)
+            cv2.waitKey(0)
+
+        # Convert the image from 'BGR' to HSV colour space
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        if debug:
+            cv2.imshow('cvtColour', image)
+            cv2.waitKey(0)
+
+        # We want to extract the 'Hue', or colour, from the image. The 'inRange'
+        # method will extract the colour we are interested in (between 0 and 180)
+        # In testing, the Hue value for red is between 95 and 125
+        # Green is between 50 and 75
+        # Blue is between 20 and 35
+        # Yellow is... to be found!
+        imrange = cv2.inRange(image, np.array(ranges[colour]["low"]), np.array(ranges[colour]["high"]))
+
+        # I used the following code to find the approximate 'hue' of the ball in
+        # front of the camera
+        #        for crange in range(0,170,10):
+        #            imrange = cv2.inRange(image, numpy.array((crange, 64, 64)), numpy.array((crange+10, 255, 255)))
+        #            print(crange)
+        #            cv2.imshow('range',imrange)
+        #            cv2.waitKey(0)
+
+        # View the filtered image found by 'imrange'
+        if debug:
+            cv2.imshow('imrange', imrange)
+            cv2.waitKey(0)
+
+        # Find the contours
+        contourimage, contours, hierarchy = cv2.findContours(imrange, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        if debug:
+            cv2.imshow('contour', contourimage)
+            cv2.waitKey(0)
+
+        # Go through each contour
+        foundArea = -1
+        foundX = -1
+        foundY = -1
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            cx = x + (w / 2)
+            cy = y + (h / 2)
+            area = w * h
+            if foundArea < area:
+                foundArea = area
+                foundX = cx
+                foundY = cy
+        if foundArea > 0:
+            ball = [foundX, foundY, foundArea]
+        else:
+            ball = None
+        print(ball)
 
