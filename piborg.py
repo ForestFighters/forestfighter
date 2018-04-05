25461054aa785480ada745041069e0167e979aaf
Godley <charlotte.godley@hotmail.co.uk>
2018-04-05 12:21:52 +0100

    fix: put back piborg class

--- piborg.py
+++ piborg.py
@@ -203,7 +203,30 @@ class StreamProcessor(threading.Thread):
 #        TB.SetMotor2(driveRight)
 
 # Image capture thread
+class ImageCapture(threading.Thread):
+    def __init__(self):
+        super(ImageCapture, self).__init__()
+        self.start()
 
+    def run(self):
+        global camera
+        global processor
+        print('Start the stream using the video port')
+        camera.capture_sequence(self.TriggerStream(), format='bgr', use_video_port=True)
+        print('Terminating camera processing...')
+        processor.terminated = True
+        processor.join()
+        print('Processing terminated.')
+
+    # Stream delegation loop
+    def TriggerStream(self):
+        global running
+        while running:
+            if processor.event.is_set():
+                time.sleep(0.01)
+            else:
+                yield processor.stream
+                processor.event.set()
 
 
 # Startup sequence
