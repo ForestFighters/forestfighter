3f365a2e4ea743042b83340e4644f431a84116e3
Godley <charlotte.godley@hotmail.co.uk>
2018-04-05 13:19:45 +0100

    further debugging work including cv2

--- rainbow.py
+++ rainbow.py
@@ -1,5 +1,8 @@
 import enum
-from picamera import PiCamera
+import time
+import picamera
+import numpy as np
+import cv2
 from time import sleep
 import logging
 
@@ -14,17 +17,22 @@ class Colours(enum.Enum):
     YELLOW = 3
     GREEN = 4
 
-ranges = {Colours.RED: {"hue": (0, 10), "sat": (158,255), "variance": (158,255)},
-          Colours.RED1: {"hue": (150, 230), "sat": (128,255), "variance": (0,255)},
-          Colours.BLUE: {"hue": (35,75), "sat": (127,255), "variance": (127,255)},
-          Colours.YELLOW: {"hue": (75,107), "sat": (127,255), "variance": (127,255)},
-          Colours.GREEN: {"hue": (20,35), "sat": (85,255), "variance": (150,255)}}
+
+ranges = {Colours.RED: {"low": (0, 158, 158), "high": (10, 255, 255)},
+          Colours.RED1: {"low": (150, 128, 0), "high": (230, 255, 255)},
+          Colours.BLUE: {"low": (35,127, 127), "high": (75, 255, 255)},
+          Colours.YELLOW: {"low": (75,127, 127), "high": (107, 255, 255)},
+          Colours.GREEN: {"low": (20,85,150), "high": (35, 255, 255)}}
 
 class Rainbow(object):
     order = []
     def __init__(self):
-        self.camera = PiCamera()
-        self.camera.zoom = (0, 0, 0.5, 0.5)
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (640, 480)
+        self.camera.brightness = 50
+        self.camera.framerate = 24
+        # warm up
+        time.sleep(2)
         # Camera.set(CV_CAP_PROP_FORMAT, CV_8UC3);
         # Camera.set(CV_CAP_PROP_FRAME_WIDTH, 640); // 320
         # Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480); // 240
@@ -40,6 +48,13 @@ class Rainbow(object):
 
     def rainbow(self):
         for colour in Colours:
+            image = np.empty((640 * 480 * 3,), dtype=np.uint8)
+            self.camera.capture(image, 'bgr')
+            image = image.reshape((640, 480, 3))
+            range = cv2.inRange(image, np.array(ranges[colour]["low"]), np.array(ranges[colour]["high"]))
+            cv2.imshow(range)
+            cv2.waitKey(0)
+            # eroded = cv2.erode(range, )
             LOGGER.debug(colour)
             sleep(2)
             #self.camera.capture('foo.jpg')
